432c8336fc35c70f5828ac7d4234d007263e6649
BDenisss <bucspun.d@gmail.com>
2024-03-20 14:08:14 +0100

    :sparkles: exo 4,5 et 6

--- pages/players/_slug.vue
+++ pages/players/_slug.vue
@@ -1,14 +1,28 @@
 
 <template>
     <div v-if="player" class="player-page">
-        <h1>{{ player.first_name }} {{ player.last_name }}</h1>
 
-        <img v-if="player.image" :src="player.image.url" :alt="`Image de ${player.first_name} ${player.last_name}`">
+        <div class="player-container">
+                <div v-if="player.image" class="player-image">
+                    <img :src="player.image.url" :alt="`Image de ${player.first_name} ${player.last_name}`">
+                </div>
+
+            <div class="player-details">
+                <div>
+                    <h1>{{ player.first_name }} {{ player.last_name }}</h1>
+                </div>
+                <div>
+                    <p><strong>Âge :</strong> {{ player.age }}</p>
+                    <p><strong>Pays :</strong> {{ player.country }}</p>
+                    <p><strong>Classement :</strong> {{ player.ranking }}</p>
+                    <p><strong>Style :</strong> {{ player.style }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
 export default {
     async asyncData({ params, $axios }) {
         try {
@@ -17,16 +31,49 @@ export default {
             return { player: data.data[0] }; 
         } catch (error) {
             console.error('Erreur lors de la récupération des détails du joueur:', error);
-            // Ceci empêchera l'erreur si la page est chargée avec des données manquantes
             return { player: null };
         }
     }
 }
-
-
-
 </script>
 
 <style scoped>
 
-</style>s
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+h1 {
+    text-align: center;
+}
+
+.player-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+}
+.player-details {
+    display: flex;
+    flex-direction: column;
+
+    gap: 16px;
+}
+
+.player-image img {
+    width: 200px; 
+    height: auto;
+    border-radius: 10px; 
+}
+
+.player-details {
+    margin-top: 20px;
+}
+
+.player-details p {
+    margin: 5px 0; 
+}
+</style>
--- pages/players/index.vue
+++ pages/players/index.vue
@@ -2,16 +2,63 @@
 
 <template>
     <div class="players-page">
-        <h1>Les joueurs</h1>
-        <DataTable :value="players" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 100%" class="table">
-            <Column field="first_name" header="First Name"></Column>
-            <Column field="last_name" header="Last Name"></Column>
-            <Column header="Profile">
-                <template #body="slotProps">
-                    <nuxt-link :to="`/players/${slotProps.data.slug}`">View Profile</nuxt-link>
+        <div class="card">
+            <DataTable :value="players" :paginator="true" :rows="10" dataKey="id" :filters.sync="filters" filterDisplay="menu" :loading="loading" responsiveLayout="scroll"
+                :globalFilterFields="['first_name', 'last_name', 'ranking', 'country']">
+                <template #header>
+                    <div class="flex justify-content-between">
+                        <Button type="button" icon="pi pi-filter-slash" label="Clear" class="p-button-outlined" @click="clearFilter"/>
+                        <span class="p-input-icon-left">
+                            <i class="pi pi-search" />
+                            <InputText v-model="filters['global'].value" placeholder="Recherche par mot-clé" />
+                        </span>
+                    </div>
                 </template>
-            </Column>
-        </DataTable>
+                <template #empty>
+                    No players found.
+                </template>
+                <template #loading>
+                    Loading players data. Please wait.
+                </template>
+                <Column field="first_name" header="Prénom" :styles="{'min-width':'12rem'}" sortable>
+                    <template #body="{data}">
+                        {{ data.first_name }}
+                    </template>
+                    <template #filter="{filterModel}">
+                        <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Search by first name"/>
+                    </template>
+                </Column>
+                <Column field="last_name" header="Nom" :styles="{'min-width':'12rem'}" sortable>
+                    <template #body="{data}">
+                        {{ data.last_name }}
+                    </template>
+                    <template #filter="{filterModel}">
+                        <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Search by last name"/>
+                    </template>
+                </Column>
+                <Column field="ranking" header="Ranking" :styles="{'min-width':'12rem'}" sortable>
+                    <template #body="{data}">
+                        {{ data.ranking }}
+                    </template>
+                    <template #filter="{filterModel}">
+                        <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Search by ranking"/>
+                    </template>
+                </Column>
+                <Column field="country" header="Pays" :styles="{'min-width':'12rem'}" sortable>
+                    <template #body="{data}">
+                        {{ data.country }}
+                    </template>
+                    <template #filter="{filterModel}">
+                        <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Search by country"/>
+                    </template>
+                </Column>
+                <Column header="Profil" :styles="{'min-width':'8rem'}">
+                    <template #body="{data}">
+                        <router-link :to="`/players/${data.slug}`" class="profile">Voir le Profil</router-link>
+                    </template>
+                </Column>
+            </DataTable>
+        </div>
     </div>
 </template>
 
@@ -20,33 +67,85 @@
 
 <script>
 
-import Vue from 'vue';
-import PrimeVue from 'primevue/config';
+import { ref, onMounted, Vue } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import InputText from 'primevue/inputtext';
+import { FilterMatchMode, FilterOperator } from 'primevue/api';
+import Button from 'primevue/button';
 
-Vue.use(PrimeVue);
-Vue.component('DataTable', DataTable);
-Vue.component('Column', Column);
 
 export default {
-    async asyncData({ $axios }) {
-        try {
-            const { data } = await $axios.get('/api/players');
-            console.log('Données récupérées (joueurs):', data.data); 
-            return { players: data.data };
-        } catch (error) {
-            console.error('Erreur lors de la récupération des joueurs:', error);
-            return { players: [] };
+    components: {
+        DataTable,
+        Column,
+        Button,
+        InputText
+    },
+    data() {
+        return {
+            filters: {
+                global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+                ranking: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                first_name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                last_name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                country: { value: null, matchMode: FilterMatchMode.STARTS_WITH }
+            },
+            players: []
+        };
+    },
+    methods: {
+        clearFilter() {
+            this.filters = {
+                global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+                ranking: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                first_name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                last_name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                country: { value: null, matchMode: FilterMatchMode.STARTS_WITH }
+            };
         }
+    },
+    async asyncData({ $axios }) {
+    try {
+        const response = await $axios.get('/api/players');
+        return { players: response.data.data || [] }; // Assurez-vous que cela renvoie toujours un tableau
+    } catch (error) {
+        console.error('Erreur lors de la récupération des joueurs:', error);
+        return { players: [] }; // Retournez un tableau vide en cas d'erreur
     }
 }
 
-
+}
 </script>
 
 <style>
 .table {
+    width: 100%;
+}
+
+.profile {
+    position: relative;
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+    padding: 5px 10px;
+    border-radius: 5px;
+    transition: all 0.3s ease-in-out;
+}   
+
+.profile::before {
+    content: '';
+    background:  black;
+    display: block;
+    position: absolute;
+    bottom: -3px;
+    left: 0;
+    width: 0;
+    height: 2px;
+    transition: all 0.3s ease-in-out;
+}
+
+.profile:hover::before {
     width: 100%;
 }
 
